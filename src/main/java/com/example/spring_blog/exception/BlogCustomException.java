9076708a3e66572bdba49aa07f9d52b512a3de63
wooKwnagwoo <bizplus0520@gmail.com>
2022-10-19 14:27:41 +0900

    log4j 커스텀

--- src/main/java/com/example/spring_blog/exception/BlogCustomException.java
+++ src/main/java/com/example/spring_blog/exception/BlogCustomException.java
@@ -5,7 +5,7 @@ import org.apache.log4j.Level;
 import org.apache.log4j.Logger;
 
 public class BlogCustomException extends RuntimeException{
-    private Logger logger = Logger.getLogger("log4j.properties");
+    private Logger logger = Logger.getLogger("log4j.appender.userLog");
     public BlogCustomException(String message, Level level) {
         super(message);
         logger.log(level,message);
